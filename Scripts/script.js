3d2a7ae5f6d4b3d0ecc3b3ab810af1494ed6bb1c
Jayson <65431625+jaysonstn@users.noreply.github.com>
2025-03-10 19:43:00 -0300

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -5,6 +5,8 @@
 document.getElementById("contato-form").addEventListener("submit", function(event) {
     event.preventDefault();
 
+    let msg = document.getElementById("msgErro") //Recupera div vazia no html
+    
     // Captura os valores dos inputs
     var templateParams = {
         from_name: document.getElementById("nome").value,  // Nome do remetente
@@ -15,10 +17,20 @@ document.getElementById("contato-form").addEventListener("submit", function(even
     // Enviar via EmailJS
     emailjs.send("service_o6lp9yi", "template_c59rdmm", templateParams) //Service ID e Template ID
     .then(function(response) {
-        alert("✅E-mail enviado com sucesso!");
+        msg.innerText = "E-mail enviado com sucesso!";
+        msg.style.color = "green";
+        msg.style.textAlign = "center";
+        msg.style.marginBottom = "10px";
+        
+        //alert("✅E-mail enviado com sucesso!");
         document.getElementById("contato-form").reset();
     }, function(error) {
-        alert("❌Erro ao enviar e-mail: " + JSON.stringify(error));
+        msg.innerText = "Falha ao enviar e-mail!";
+        msg.style.color = "red";
+        msg.style.textAlign = "center";
+        msg.style.marginBottom = "10px";
+        
+        //alert("❌Erro ao enviar e-mail: " + JSON.stringify(error));
     });
 });
 
