127b78c630c83591b9bb055c5fa869d72400065c
Jayson <65431625+jaysonstn@users.noreply.github.com>
2025-05-01 15:19:15 -0300

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,40 +1,32 @@
+
+
+
 (function(){
-    emailjs.init("rTs86hwBaSaesZM1C"); //  User ID do EmailJS
+    emailjs.init("rTs86hwBaSaesZM1C"); // Substitua com seu user ID do EmailJS
 })();
 
 document.getElementById("contato-form").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    let msg = document.getElementById("msgErro") //Recupera div vazia no html
-    
-    // Captura os valores dos inputs
+    let msg = document.getElementById("msgErro");
+
     var templateParams = {
-        from_name: document.getElementById("nome").value,  // Nome do remetente
-        from_email: document.getElementById("email").value, // E-mail do remetente
-        message: document.getElementById("mensagem").value // Mensagem do usuário
+        from_name: document.getElementById("nome").value,
+        from_email: document.getElementById("email").value,
+        message: document.getElementById("mensagem").value
     };
 
-    // Enviar via EmailJS
     emailjs.send("service_o6lp9yi", "template_c59rdmm", templateParams) //Service ID e Template ID
     .then(function(response) {
         msg.innerText = "E-mail enviado com sucesso!";
         msg.style.color = "green";
         msg.style.textAlign = "center";
         msg.style.marginBottom = "10px";
-        
-        //alert("✅E-mail enviado com sucesso!");
         document.getElementById("contato-form").reset();
     }, function(error) {
         msg.innerText = "Falha ao enviar e-mail!";
         msg.style.color = "red";
         msg.style.textAlign = "center";
         msg.style.marginBottom = "10px";
-        
-        //alert("❌Erro ao enviar e-mail: " + JSON.stringify(error));
     });
 });
-
-  
-  
-  
-  
